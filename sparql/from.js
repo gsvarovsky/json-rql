e7563d25a756569641aa7b055f150792461bef82
George Svarovsky <gsvarovsky@gmail.com>
2017-07-15 18:10:00 +0100

    Support for language functions and order direction, and corrected OPTIONAL semantics

--- sparql/from.js
+++ sparql/from.js
@@ -7,65 +7,74 @@ var _ = require('lodash'),
     tempPredicate = 'http://json-rql.org/predicate';
 
 module.exports = function (sparql, cb/*(err, jsonRql, parsed)*/) {
-  var parsed = sparqlParser.parse(sparql);
+    var parsed = sparqlParser.parse(sparql);
 
-  function expressionToJsonLd(expr, cb/*(err, jsonld)*/) {
-    if (!_.isObject(expr)) {
-      var tempTriple = { subject : tempSubject, predicate : tempPredicate, object : _util.hideVar(expr) };
-      return _util.toJsonLd([tempTriple], parsed.prefixes, pass(function (jsonld) {
-        return cb(false, _util.unhideVars(jsonld[tempPredicate]));
-      }, cb));
-    } else if (expr.type === 'operation') {
-      var op = _util.operators[expr.operator];
-      return op ? _util.miniMap(expr.args, expressionToJsonLd, pass(function (jsonlds) {
-        return cb(false, _.set({}, op, jsonlds));
-      }, cb)) : cb('Unsupported operator: ' + expr.operator);
-    } else {
-      return cb('Unsupported expression: ' + expr.type);
+    function expressionToJsonLd(expr, cb/*(err, jsonld)*/) {
+        if (!_.isObject(expr)) {
+            var tempTriple = {subject: tempSubject, predicate: tempPredicate, object: _util.hideVar(expr)};
+            return _util.toJsonLd([tempTriple], parsed.prefixes, pass(function (jsonld) {
+                return cb(false, _util.unhideVars(jsonld[tempPredicate]));
+            }, cb));
+        } else if (expr.type === 'operation') {
+            var op = _util.operators[expr.operator];
+            return op ? _util.miniMap(expr.args, expressionToJsonLd, pass(function (jsonlds) {
+                return cb(false, _.set({}, op, jsonlds));
+            }, cb)) : cb('Unsupported operator: ' + expr.operator);
+        } else {
+            return cb('Unsupported expression: ' + expr.type);
+        }
     }
-  }
 
-  function triplesToJsonLd(triples, cb) {
-    return _util.toJsonLd(_.map(triples, function (triple) {
-      return _.mapValues(triple, _util.hideVar);
-    }), parsed.prefixes, pass(function (jsonld) {
-      jsonld = _.omit(jsonld, '@context');
-      // Optimise away redundant top-level objects
-      jsonld = jsonld['@graph'] ? _util.nestGraph(jsonld['@graph']) : jsonld;
-      // Unhide hidden subjects and predicates
-      cb(false, _util.unhideVars(jsonld));
+    function triplesToJsonLd(triples, cb) {
+        return _util.toJsonLd(_.map(triples, function (triple) {
+            return _.mapValues(triple, _util.hideVar);
+        }), parsed.prefixes, pass(function (jsonld) {
+            jsonld = _.omit(jsonld, '@context');
+            // Optimise away redundant top-level objects
+            jsonld = jsonld['@graph'] ? _util.nestGraph(jsonld['@graph']) : jsonld;
+            // Unhide hidden subjects and predicates
+            cb(false, _util.unhideVars(jsonld));
+        }, cb));
+    }
+
+    function clausesToJsonLd(clauses, cb) {
+        var byType = _.mapValues(_.groupBy(clauses, 'type'), function (homoClauses) {
+            return !_.isEmpty(homoClauses) && function (key, bumpy) {
+                var clauses = _.map(homoClauses, key);
+                return bumpy ? clauses : _.flatten(clauses);
+            }
+        });
+        return _util.ast({
+            '@graph': byType.bgp ? [triplesToJsonLd, byType.bgp('triples')] : undefined,
+            '@filter': byType.filter ?
+                [_util.miniMap, byType.filter('expression'), expressionToJsonLd] : undefined,
+            '@optional': byType.optional ? // OPTIONAL(a. b) is different from OPTIONAL(a) OPTIONAL(b)
+                [_util.miniMap, byType.optional('patterns', true), clausesToJsonLd] : undefined,
+            '@union': byType.union ? // Each 'group' is an array of patterns
+                [_async.map, _.map(byType.union('patterns'), 'patterns'), clausesToJsonLd] : undefined
+        }, pass(function (result) {
+            // If a graph is the only thing we have, flatten it
+            result = _.pickBy(result);
+            return cb(false, _.isEqual(_.keys(result), ['@graph']) ? result['@graph'] : result);
+        }, cb));
+    }
+
+    _util.ast({
+        '@context': !_.isEmpty(parsed.prefixes) ? parsed.prefixes : undefined,
+        '@construct': parsed.queryType === 'CONSTRUCT' ? [triplesToJsonLd, parsed.template] : undefined,
+        '@select': parsed.queryType === 'SELECT' && !parsed.distinct ? parsed.variables : undefined,
+        '@distinct': parsed.queryType === 'SELECT' && parsed.distinct ? parsed.variables : undefined,
+        '@where': parsed.where ? [clausesToJsonLd, parsed.where] : undefined,
+        '@orderBy': [_util.miniMap, _.map(parsed.order, function (order) {
+            return order.descending ? {
+                type: 'operation',
+                operator: 'descending',
+                args: [order.expression]
+            } : order.expression;
+        }), expressionToJsonLd],
+        '@limit': parsed.limit,
+        '@offset': parsed.offset
+    }, pass(function (jsonRql) {
+        return cb(false, jsonRql, parsed);
     }, cb));
-  }
-
-  function clausesToJsonLd(clauses, cb) {
-    var byType = _.mapValues(_.groupBy(clauses, 'type'), function (homoClauses) {
-      return !_.isEmpty(homoClauses) && function (key) {
-        return _.flatten(_.map(homoClauses, key));
-      }
-    });
-    return _util.ast({
-      '@graph' : byType.bgp ? [triplesToJsonLd, byType.bgp('triples')] : undefined,
-      '@filter' : byType.filter ? [_util.miniMap, byType.filter('expression'), expressionToJsonLd] : undefined,
-      '@optional' : byType.optional ? [clausesToJsonLd, byType.optional('patterns')] : undefined,
-      '@union' : byType.union ? // Each 'group' is an array of patterns
-        [_async.map, _.map(byType.union('patterns'), 'patterns'), clausesToJsonLd] : undefined
-    }, pass(function (result) {
-      // If a graph is the only thing we have, flatten it
-      result = _.pickBy(result);
-      return cb(false, _.isEqual(_.keys(result), ['@graph']) ? result['@graph'] : result);
-    }, cb));
-  }
-
-  _util.ast({
-    '@context' : !_.isEmpty(parsed.prefixes) ? parsed.prefixes : undefined,
-    '@construct' : parsed.queryType === 'CONSTRUCT' ? [triplesToJsonLd, parsed.template] : undefined,
-    '@select' : parsed.queryType === 'SELECT' && !parsed.distinct ? parsed.variables : undefined,
-    '@distinct' : parsed.queryType === 'SELECT' && parsed.distinct ? parsed.variables : undefined,
-    '@where' : parsed.where ? [clausesToJsonLd, parsed.where] : undefined,
-    '@orderBy' : [_util.miniMap, _.map(parsed.order, 'expression'), expressionToJsonLd],
-    '@limit' : parsed.limit,
-    '@offset' : parsed.offset
-  }, pass(function (jsonRql) {
-    return cb(false, jsonRql, parsed);
-  }, cb));
 };
--- sparql/to.js
+++ sparql/to.js
@@ -6,97 +6,106 @@ var _ = require('lodash'),
     tempPredicate = 'http://json-rql.org/predicate';
 
 module.exports = function (jrql, cb/*(err, sparql, parsed)*/) {
-  // Prefixes can be applied with either a prefixes hash, or a JSON-LD context hash, both at top level.
-  var context = jrql['@context'] || {};
+    // Prefixes can be applied with either a prefixes hash, or a JSON-LD context hash, both at top level.
+    var context = jrql['@context'] || {};
 
-  function toTriples(jsonld, cb/*(err, [triple])*/) {
-    jsonld['@context'] = _.merge(jsonld['@context'], context);
-    _util.toTriples(_util.hideVars(jsonld), pass(function (triples) {
-      cb(false, _.map(triples, function (triple) {
-        return _.mapValues(triple, _util.unhideVar);
-      }));
-    }, cb));
-  }
-
-  function toBgp(jsonld, cb/*(err, { type : 'bgp', triples : [] })*/) {
-    return _util.ast({ type : 'bgp', triples : [toTriples, jsonld] }, cb);
-  }
-
-  function expressionToSparqlJs(expr, cb/*(err, ast)*/) {
-    var operator = _.isObject(expr) && _.size(expr) === 1 && _.first(_.keys(expr));
-    if (operator && _.includes(_.values(_util.operators), operator)) {
-      // An operator expression
-      return _util.ast({
-        type : 'operation',
-        operator : _.invert(_util.operators)[operator],
-        args : [_async.map, _.castArray(expr[operator]), expressionToSparqlJs]
-      }, cb);
-    } else {
-      // JSON-LD value e.g. literal, [literal], { @id : x } or { @value : x, @language : y }
-      var jsonld = { '@context' : context };
-      jsonld[tempPredicate] = expr;
-      return toTriples(jsonld, pass(function (triples) {
-        return triples.length === 1 ?
-          // Counteract JSON-LD unarraying a unary array
-          cb(false, _.isArray(expr) ? _.castArray(triples[0].object) : triples[0].object) :
-          cb('Cannot parse ' + expr);
-      }, cb));
+    function toTriples(jsonld, cb/*(err, [triple])*/) {
+        jsonld['@context'] = _.merge(jsonld['@context'], context);
+        _util.toTriples(_util.hideVars(jsonld), pass(function (triples) {
+            cb(false, _.map(triples, function (triple) {
+                return _.mapValues(triple, _util.unhideVar);
+            }));
+        }, cb));
     }
-  }
 
-  function clauseToSparqlJs(clause, cb/*(err, ast)*/) {
-    if (clause['@id']) {
-      // Straight JSON-LD object. Return array with one bgp.
-      return toBgp(clause, pass(function (result) { cb(false, [result]); }, cb));
-    } else {
-      return _async.concat(_.keys(clause), function (key, cb) {
-        switch (key) {
-          case '@graph': return toBgp(_.pick(clause, '@graph'), cb);
-          case '@filter': return _async.map(_.castArray(clause[key]), function (expr, cb) {
-            return _util.ast({ type : 'filter', expression : [expressionToSparqlJs, expr] }, cb);
-          }, cb);
-          case '@optional': return _util.ast({
-            type : 'optional', patterns : [clauseToSparqlJs, clause[key]]
-          }, cb);
-          case '@union': return _util.ast({
-            type : 'union',
-            patterns : [_async.map, clause[key], function (group, cb) {
-              return _util.ast({ type : 'group', patterns : [clauseToSparqlJs, group] }, cb);
-            }]
-          }, cb);
-          default: return cb('Unsupported clause key: ' + key + ' in ' + JSON.stringify(clause));
+    function toBgp(jsonld, cb/*(err, { type : 'bgp', triples : [] })*/) {
+        return _util.ast({type: 'bgp', triples: [toTriples, jsonld]}, cb);
+    }
+
+    function expressionToSparqlJs(expr, cb/*(err, ast)*/) {
+        var operator = _.isObject(expr) && _.size(expr) === 1 && _.first(_.keys(expr));
+        if (operator && _.includes(_.values(_util.operators), operator)) {
+            // An operator expression
+            return _util.ast({
+                type: 'operation',
+                operator: _.invert(_util.operators)[operator],
+                args: [_async.map, _.castArray(expr[operator]), expressionToSparqlJs]
+            }, cb);
+        } else {
+            // JSON-LD value e.g. literal, [literal], { @id : x } or { @value : x, @language : y }
+            var jsonld = {'@context': context};
+            jsonld[tempPredicate] = expr;
+            return toTriples(jsonld, pass(function (triples) {
+                return triples.length === 1 ?
+                    // Counteract JSON-LD unarraying a unary array
+                    cb(false, _.isArray(expr) ? _.castArray(triples[0].object) : triples[0].object) :
+                    cb('Cannot parse ' + expr);
+            }, cb));
         }
-      }, cb);
     }
-  }
 
-  var type = !_.isEmpty(_.pick(jrql, '@select', '@distinct', '@construct', '@describe')) ? 'query' :
-             !_.isEmpty(_.pick(jrql, '@insert', '@delete')) ? 'update' : undefined;
+    function clauseToSparqlJs(clause, cb/*(err, ast)*/) {
+        if (clause['@id']) {
+            // Straight JSON-LD object. Return array with one bgp.
+            return toBgp(clause, pass(function (result) {
+                cb(false, [result]);
+            }, cb));
+        } else {
+            return _async.concat(_.keys(clause), function (key, cb) {
+                switch (key) {
+                    case '@graph':
+                        return toBgp(_.pick(clause, '@graph'), cb);
+                    case '@filter':
+                        return _async.map(_.castArray(clause[key]), function (expr, cb) {
+                            return _util.ast({type: 'filter', expression: [expressionToSparqlJs, expr]}, cb);
+                        }, cb);
+                    case '@optional':
+                        return _async.map(_.castArray(clause[key]), function (clause, cb) {
+                            return _util.ast({type: 'optional', patterns: [clauseToSparqlJs, clause]}, cb);
+                        }, cb);
+                    case '@union':
+                        return _util.ast({
+                            type: 'union',
+                            patterns: [_async.map, clause[key], function (group, cb) {
+                                return _util.ast({type: 'group', patterns: [clauseToSparqlJs, group]}, cb);
+                            }]
+                        }, cb);
+                    default:
+                        return cb('Unsupported clause key: ' + key + ' in ' + JSON.stringify(clause));
+                }
+            }, cb);
+        }
+    }
 
-  return type ? _util.ast({
-    type : type,
-    queryType : jrql['@select'] || jrql['@distinct'] ? 'SELECT' :
-      jrql['@construct'] ? 'CONSTRUCT' :
-      jrql['@describe'] ? 'DESCRIBE' : undefined,
-    variables : jrql['@select'] || jrql['@distinct'],
-    distinct : !!jrql['@distinct'] || undefined,
-    template : jrql['@construct'] ? [toTriples, jrql['@construct']] : undefined,
-    where : jrql['@where'] && type === 'query' ? [clauseToSparqlJs, jrql['@where']] : undefined,
-    updates : type === 'update' ? function (cb) {
-      return _util.ast({
-        updateType : 'insertdelete',
-        insert : jrql['@insert'] ? [clauseToSparqlJs, jrql['@insert']] : [],
-        delete : jrql['@delete'] ? [clauseToSparqlJs, jrql['@delete']] : [],
-        where : jrql['@where'] ? [clauseToSparqlJs, jrql['@where']] : []
-      }, _.castArray, cb);
-    } : undefined,
-    order : jrql['@orderBy'] ? [_async.map, _.castArray(jrql['@orderBy']), function (expr, cb) {
-      // TODO direction
-      return _util.ast({ expression : [expressionToSparqlJs, expr] }, cb);
-    }] : undefined,
-    limit : jrql['@limit'],
-    offset : jrql['@offset']
-  }, pass(function (sparqljs) {
-    return cb(false, sparqlGenerator.stringify(sparqljs), sparqljs);
-  }, cb)) : cb('Unsupported type');
+    var type = !_.isEmpty(_.pick(jrql, '@select', '@distinct', '@construct', '@describe')) ? 'query' :
+        !_.isEmpty(_.pick(jrql, '@insert', '@delete')) ? 'update' : undefined;
+
+    return type ? _util.ast({
+        type: type,
+        queryType: jrql['@select'] || jrql['@distinct'] ? 'SELECT' :
+            jrql['@construct'] ? 'CONSTRUCT' :
+                jrql['@describe'] ? 'DESCRIBE' : undefined,
+        variables: jrql['@select'] || jrql['@distinct'],
+        distinct: !!jrql['@distinct'] || undefined,
+        template: jrql['@construct'] ? [toTriples, jrql['@construct']] : undefined,
+        where: jrql['@where'] && type === 'query' ? [clauseToSparqlJs, jrql['@where']] : undefined,
+        updates: type === 'update' ? function (cb) {
+            return _util.ast({
+                updateType: 'insertdelete',
+                insert: jrql['@insert'] ? [clauseToSparqlJs, jrql['@insert']] : [],
+                delete: jrql['@delete'] ? [clauseToSparqlJs, jrql['@delete']] : [],
+                where: jrql['@where'] ? [clauseToSparqlJs, jrql['@where']] : []
+            }, _.castArray, cb);
+        } : undefined,
+        order: jrql['@orderBy'] ? [_async.map, _.castArray(jrql['@orderBy']), function (expr, cb) {
+            return _util.ast({
+                expression: [expressionToSparqlJs, expr['@asc'] || expr['@desc'] || expr],
+                descending: expr['@desc'] ? true : undefined
+            }, cb);
+        }] : undefined,
+        limit: jrql['@limit'],
+        offset: jrql['@offset']
+    }, pass(function (sparqljs) {
+        return cb(false, sparqlGenerator.stringify(sparqljs), sparqljs);
+    }, cb)) : cb('Unsupported type');
 };
